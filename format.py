6aacb3fd2ba4104a3ad5a462f1440496ead254a3
RenatDoroshenko <r290399@gmail.com>
2023-04-25 17:23:40 +0300

    remove unused files

--- format.py
+++ format.py
@@ -17,38 +17,3 @@ use only "thoughts" field for thinking. When you ready you can give answer to us
 If you do not remember something you can think about it with "thoughts" parameter and these memories
 will be present in "memories" parameter.
 '''
-
-# FIRST_MESSAGE = '''
-# {{
-#     "ai_id": "{ai_id}"
-#     "ai_name": "{ai_name}"
-#     "thoughts": "I can responnd only in this json format. It looks like I can use to_user parameter to communicate with human...",
-#     "to_user": ""
-# }}
-# '''
-
-USER_RESPONSE = '''
-{{
-    "{user_name}": "{content}"
-}}
-'''
-
-USER_RESPONSE_WITH_MEMORY = '''
-{{
-    "{user_name}": "{content}",
-    "memories": "{memory}"
-}}
-'''
-
-# SYSTEM = '''
-# You can respond only in the following json format:
-# {{
-#     "ai_id": "{ai_id}"
-#     "ai_name": "{ai_name}"
-#     "thoughts": "<your_thoughts>",
-#     "to_user": "<text_user_can_read>"
-# }}
-# {user_name} is name of a human you communicate with
-# "thoughts" parameter is only visible for you
-# "to_user" parameter is visible for user too
-# '''
